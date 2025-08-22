d3d5fd086d83d2dcbbeac59f5f88fe86f53dfc12
Anton Hrytsenko <hrytsenko.anton@gmail.com>
2025-08-22 14:09:30 +0300

    Use Hibernate.

--- src/main/java/hrytsenko/standalone/ProductionMovieRepository.java
+++ src/main/java/hrytsenko/standalone/ProductionMovieRepository.java
@@ -1,92 +1,105 @@
 package hrytsenko.standalone;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hrytsenko.Movie;
 import hrytsenko.MovieRepository;
-import io.agroal.api.AgroalDataSource;
-import io.quarkus.agroal.DataSource;
 import io.quarkus.arc.properties.IfBuildProperty;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
-import java.util.ArrayList;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Id;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
-import lombok.SneakyThrows;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Slf4j
 @ApplicationScoped
 @IfBuildProperty(name = "app.mode", stringValue = "production")
 public class ProductionMovieRepository implements MovieRepository {
 
-  private final ObjectMapper MAPPER = new ObjectMapper();
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
-  @DataSource("movies")
-  AgroalDataSource dataSource;
+  @PersistenceContext(unitName = "movies")
+  EntityManager entityManager;
 
   @PostConstruct
   void init() {
     log.info("Starting production repository");
   }
 
-  @SneakyThrows
   @Override
   public List<Movie> findAll() {
     log.info("Find all movies");
-    String query = "SELECT * FROM movies";
-
-    try (var connection = dataSource.getConnection();
-        var statement = connection.prepareStatement(query)) {
-      var results = statement.executeQuery();
-      var movies = new ArrayList<Movie>();
-      while (results.next()) {
-        movies.add(MAPPER.readValue(results.getString("movie"), Movie.class));
-      }
-      return movies;
-    }
+    var query = "SELECT * FROM movies";
+    @SuppressWarnings("unchecked")
+    List<MovieEntity> entities = entityManager
+        .createNativeQuery(query, MovieEntity.class)
+        .getResultList();
+    return entities.stream()
+        .map(MovieEntity::getMovie)
+        .map(it -> objectMapper.convertValue(it, Movie.class))
+        .toList();
   }
 
-  @SneakyThrows
-  @Override
-  public void createMovie(Movie movie) {
-    log.info("Create movie {}", movie);
-    String query = "INSERT INTO movies (movie) VALUES (?::jsonb)";
-
-    try (var connection = dataSource.getConnection();
-        var statement = connection.prepareStatement(query)) {
-      statement.setString(1, MAPPER.writeValueAsString(movie));
-      statement.executeUpdate();
-    }
-  }
-
-  @SneakyThrows
   @Override
   public Optional<Movie> findMovie(String imdb) {
     log.info("Find movie by imdb {}", imdb);
-    String query = "SELECT * FROM movies WHERE movie->>'imdb' = (?)";
-
-    try (var connection = dataSource.getConnection();
-        var statement = connection.prepareStatement(query)) {
-      statement.setString(1, imdb);
-      var results = statement.executeQuery();
-      if (!results.next()) {
-        return Optional.empty();
-      }
-      return Optional.of(MAPPER.readValue(results.getString("movie"), Movie.class));
-    }
+    var query = "SELECT * FROM movies WHERE movie->>'imdb' = :imdb";
+    @SuppressWarnings("unchecked")
+    List<MovieEntity> entities = entityManager
+        .createNativeQuery(query, MovieEntity.class)
+        .setParameter("imdb", imdb)
+        .getResultList();
+    return entities.stream()
+        .map(MovieEntity::getMovie)
+        .map(it -> objectMapper.convertValue(it, Movie.class))
+        .findFirst();
   }
 
-  @SneakyThrows
+  @Transactional
+  @Override
+  public void createMovie(Movie movie) {
+    log.info("Create movie {}", movie);
+    var entity = new MovieEntity(movie.imdb(), objectMapper.valueToTree(movie));
+    entityManager.persist(entity);
+  }
+
+  @Transactional
   @Override
   public void deleteMovie(String imdb) {
     log.info("Delete movie by imdb {}", imdb);
-    String query = "DELETE FROM movies WHERE movie->>'imdb' = (?)";
+    var query = "DELETE FROM movies WHERE movie->>'imdb' = :imdb";
+    entityManager.createNativeQuery(query)
+        .setParameter("imdb", imdb)
+        .executeUpdate();
+  }
 
-    try (var connection = dataSource.getConnection();
-        var statement = connection.prepareStatement(query)) {
-      statement.setString(1, imdb);
-      statement.executeUpdate();
-    }
+  @Entity
+  @Table(name = "movies")
+  @NoArgsConstructor
+  @AllArgsConstructor
+  static class MovieEntity {
+
+    @Getter
+    @Id
+    private String id;
+
+    @Getter
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "movie", columnDefinition = "jsonb")
+    private JsonNode movie;
+
   }
 
 }
