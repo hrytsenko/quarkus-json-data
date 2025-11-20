27b2ff73ec7ae284939021cd99de63c5534f816e
Anton Hrytsenko <hrytsenko.anton@gmail.com>
2025-11-20 13:55:32 +0200

    Run tests in standalone mode.

--- src/test/java/hrytsenko/MovieTest.java
+++ src/test/java/hrytsenko/MovieTest.java
@@ -3,59 +3,16 @@ package hrytsenko;
 import static io.restassured.RestAssured.given;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 
-import hrytsenko.MovieTest.MovieTestProfile;
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.junit.QuarkusTestProfile;
-import io.quarkus.test.junit.TestProfile;
 import jakarta.ws.rs.core.MediaType;
-import java.util.Map;
-import org.flywaydb.core.Flyway;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @QuarkusTest
-@Testcontainers
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestProfile(MovieTestProfile.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MovieTest {
-
-  @Container
-  @SuppressWarnings("resource")
-  public static PostgreSQLContainer<?> PG = new PostgreSQLContainer<>("postgres")
-      .withDatabaseName("movies")
-      .withUsername("admin")
-      .withPassword("s3cr3t");
-
-  public static class MovieTestProfile implements QuarkusTestProfile {
-
-    @Override
-    public Map<String, String> getConfigOverrides() {
-      PG.start();
-
-      return Map.of(
-          "app.database.movies.url", PG.getJdbcUrl(),
-          "app.database.movies.username", PG.getUsername(),
-          "app.database.movies.password", PG.getPassword());
-    }
-
-  }
-
-  @BeforeAll
-  public void init() {
-    Flyway.configure()
-        .dataSource(PG.getJdbcUrl(), PG.getUsername(), PG.getPassword())
-        .locations("filesystem:./migrations")
-        .load()
-        .migrate();
-  }
 
   @Test
   @Order(1)
